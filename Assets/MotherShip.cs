dd6aed8c1a95589e73a1ac66e468e034fcc1c15a
BenediktOrnHjaltason <bendikorn@gmail.com>
2020-02-27 17:26:43 +0000

    Mothership was colliding with miniscule gun

--- Assets/MotherShip.cs
+++ Assets/MotherShip.cs
@@ -8,12 +8,20 @@ public class MotherShip : MonoBehaviour
     private AudioSource ExplosionSound;
     public ParticleSystem ExplosionFX;
     public ParticleSystem SmokeFX;
+    public GameObject Mesh;
+    public GameObject Light;
+
+
     public GameObject AvatarRigBase;
+
+
 
     private float yPos = 0;
     private Vector3 NewPos;
     private float timeAtSpawn;
     private bool bStoppedRising = false;
+
+    private int health = 9;
 
     // Start is called before the first frame update
     void Start()
@@ -33,5 +41,31 @@ public class MotherShip : MonoBehaviour
         }
 
         else transform.position = new Vector3(transform.position.x, ((Mathf.Sin((Time.time * 2)) * 6) + 265.0f), transform.position.z);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.LogError("MOTHERSHIP COLLIDING WITH: " + other.name);
+
+        other.enabled = false;
+        Destroy(other);
+
+        ExplosionSound.Play();
+        ExplosionFX.Play();
+
+
+        health--;
+
+        if (health == 4) SmokeFX.Play();
+
+        else if (health < 1)
+        {
+            Destroy(Mesh);
+            Destroy(Light);
+            Destroy(SmokeFX);
+            Destroy(ExplosionFX);
+            Destroy(GetComponent<Light>());
+            Destroy(this);
+        }
     }
 }
